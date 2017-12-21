e8844c78f42158e53218eee39b44536cbbde4153
MounMaz <maz.moun@yahoo.fr>
2017-12-21 16:16:38 +0200

    Adding zombie AI logic and spawner. Adding lights

---
+++ OuluZombies Unity/Assets/CameraControl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+    public GameObject target;
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.localPosition = new Vector3(target.transform.localPosition.x+ 20, gameObject.transform.localPosition.y, gameObject.transform.localPosition.z );
+        gameObject.transform.LookAt(target.transform);
+	}
+}
